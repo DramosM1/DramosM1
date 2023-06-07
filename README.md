1e790edb5532096a5f9fd48a4364e0137b17ef19
DramosM1 <d.ramos.malebran@gmail.com>
2023-06-07 18:57:26 -0400

    commit 2do

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # marvPruebas
+# marvPruebas
--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const servidor = require("express")
+const app = servidor()
+const port =3000
+
+app.get("/", (req,res)=> {
+    res.send("servidor para pruebas")
+})
+
+app.listen(port,() =>{
+    console.log("servidor activo");
+})
+
 
 let array = {"nombre" : "david"}
 
